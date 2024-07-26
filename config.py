8e0fb20ee1f63846273d42670c1d0cbe775ca0df
Kostiantyn Ivashchenko <kivaschenko@protonmail.com>
2024-07-26 13:11:41 +0300

    update env for dev and test mode

--- config.py
+++ config.py
@@ -8,11 +8,11 @@ load_dotenv(dotenv_path)
 
 
 def get_postgres_uri():
-    host = os.environ.get("DB_HOST", "localhost")
+    host = os.environ.get("DB_HOST", "172.17.0.1")
     port = os.environ.get("DB_PORT", 5432)
-    password = os.environ.get("DB_PASSWORD")
-    user = os.environ.get("DB_USER")
-    db_name = os.environ.get("DB_NAME")
+    password = os.environ.get("DB_PASSWORD", "test_password")
+    user = os.environ.get("DB_USER", "admin")
+    db_name = os.environ.get("DB_NAME", "postgres")
     uri = f"postgresql://{user}:{password}@{host}:{port}/{db_name}"
     return uri
 
